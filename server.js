2d4e8b87c974c6ea1ce1ef0b5dbcae1924c8f973
Tushar <tusharmittal429@gmail.com>
2021-05-26 22:39:23 +0530

    added files

---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+
+const connectDB = require('./config/db');
+connectDB();
+
+// Cors 
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+<<<<<<< HEAD
+app.use(cors());
+app.options('*', cors());
+
+const connectDB = require('./config/db');
+connectDB();
+=======
+>>>>>>> 583a9df747a05cc0082e0f0e5c357a8473effce5
+
+app.use(express.static('public'));
+
+//Middlewares
+app.use(express.json());
+
+app.use(cors(corsOptions));
+
+//Template Engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/api/files', require('./routes/file'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download/', require('./routes/download'));
+
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
